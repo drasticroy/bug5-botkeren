b77c9d717b6b2953c239ffe30ed8e0898b20d6ce
Recoder Drastic <102560562+drasticroy@users.noreply.github.com>
2023-10-19 20:49:51 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,51 @@
+/**
+   * Script ori By Dika Ardnt.
+   * Recode by HenzzXD.
+   * Contact Me on wa.me/6285711324080
+   * Follow https://github.com/henzz4368
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://api.zahwazein.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://api.zahwazein.xyz': 'pasang apikey lu',
+}
+
+// Other
+global.owner = ['6285711324080']
+global.premium = ['6285711324080']
+global.packname = 'Sazumi Henzz'
+global.author = 'WhatsApp Bot'
+global.sessionName = 'hisoka'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
